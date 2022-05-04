a4fa4ad7c2f7d21ea73db9011e57a90a94aab871
Melli11 <mingerez@gmail.com>
2022-05-04 18:06:35 -0300

    Testeando

--- src/Library.hs
+++ src/Library.hs
@@ -1,9 +1,42 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+-- data Deseos = aprenderHabilidades Habilidades Chico | serGrosoEnNeedForSpeed Chico | serMayor Chico
 
-triple numero = numero*3
+type Habilidades = String
+type Deseos = Chico -> Chico
+data Chico = Chico {
+    nombre :: String,
+    edad :: Number,
+    habilidad :: [Habilidades]
+    } deriving (Show,Eq)
 
-cuadruple numero = numero*4
+-- chico = Chico {
+--     nombre = "bobi",
+--     edad = 10 ,
+--     habilidad = [],
+--     deseos = []
+--     }
+-- chicoConHabilidad  = Chico {
+--     nombre = "bobiHabilidoso",
+--     edad = 10 ,
+--     habilidad = ["Prog Haskell Super Sr"],
+--     deseos = []
+--     }
+
+-- chicoMocoso = Chico {
+--     nombre = "bobiConMocos",
+--     edad = 10 ,
+--     habilidad = ["Sacarse los mocos"],
+--     deseos = []
+--     }
+
+aprenderHabilidades :: [Habilidades] -> Chico -> Chico
+-- aprenderHabilidades nuevasHabilidades unChico = unChico {habilidad = nuevasHabilidades ++ habilidad unChico} 
+aprenderHabilidades nuevasHabilidades unChico = unChico {habilidad = nuevasHabilidades ++ habilidad unChico} 
+
+-- serGrosoEnNeedForSpeed unChico = aprenderHabilidades 
+
+serMayor unChico = unChico {edad = 18} 
+
+--timmy = Chico “Timmy” 10 [“mirar television”, “jugar en la pc”] [serMayor]
--- src/Spec.hs
+++ src/Spec.hs
@@ -2,14 +2,48 @@ module Spec where
 import PdePreludat
 import Library
 import Test.Hspec
+import Control.Exception (evaluate)
+
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
-    it "El pdepreludat se instaló correctamente" $ do
-      triple 1 `shouldBe` 3
-    it "El pdepreludat se instaló correctamente" $ do
-      cuadruple 1 `shouldBe` 4
+  suiteDeTestsDeParte1
+  -- suiteDeTestsDeParte2
+  -- suiteDeTestsDeParte3
+suiteDeTestsDeParte1 = describe "Concediendo Deseos" $ do
+-- data Chico = Chico {
+--     nombre :: String,
+--     edad :: Number,
+--     habilidad :: [Habilidades],
+--     deseos :: [Deseos]
+--     } 
+  -- let chico = Chico "bobi" 10 [] []
+  -- let chicoConHabilidad = Chico "bobi" 10 ["Prog Haskell Super Sr"] []
+  -- let chicoMocoso = Chico "bobi" 10 ["Aprender a sacarse los mocos"] []
 
+  let chico = Chico {
+    nombre = "bobi",
+    edad = 10 ,
+    habilidad = []
+    --deseos = []
+    }
+  let chicoConHabilidad  = Chico {
+    nombre = "bobi",
+    edad = 10 ,
+    habilidad = ["Prog Haskell Super Sr"]
+    --deseos = []
+    }
+
+  let chicoMocoso = Chico {
+    nombre = "bobi",
+    edad = 10 ,
+    habilidad = ["Sacarse los mocos"]
+    --deseos = []
+    }
+
+  describe "Aprender habilidades" $ do
+    it "Sea una nueva habilidad  sacarse los mocos entonces el chico aprenderá a sacarse los mocos" $ do
+      aprenderHabilidades ["Sacarse los mocos"] chico `shouldBe` chicoMocoso
+    it "Sea un chico con habilidad si no le enseño nada seguira con la misma habilidad" $ do
+      aprenderHabilidades [] chicoConHabilidad `shouldBe` chicoConHabilidad 
+    
