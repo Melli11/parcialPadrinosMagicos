2191e147b9b0f03a40b411be8c47dd88c096f848
GEREZ Martin <martin.gerez@kantaribopemedia.com>
2022-05-04 23:50:07 -0300

    Hasta el punto 2

--- src/Library.hs
+++ src/Library.hs
@@ -1,42 +1,95 @@
 module Library where
 import PdePreludat
 
--- data Deseos = aprenderHabilidades Habilidades Chico | serGrosoEnNeedForSpeed Chico | serMayor Chico
 
 type Habilidades = String
-type Deseos = Chico -> Chico
+
+type Deseo = Chico -> Chico
+
 data Chico = Chico {
     nombre :: String,
     edad :: Number,
-    habilidad :: [Habilidades]
+    habilidad :: [Habilidades],
+    deseo :: [Deseo]
     } deriving (Show,Eq)
 
--- chico = Chico {
---     nombre = "bobi",
---     edad = 10 ,
---     habilidad = [],
---     deseos = []
---     }
--- chicoConHabilidad  = Chico {
---     nombre = "bobiHabilidoso",
---     edad = 10 ,
---     habilidad = ["Prog Haskell Super Sr"],
---     deseos = []
---     }
+chico :: Chico
+chico = Chico "bobi" 10 [] [serMayor]
+    
+chicoConHabilidad :: Chico
+chicoConHabilidad  = Chico "bobiHabilidoso" 10 ["Prog Haskell Super Sr"] []
 
--- chicoMocoso = Chico {
---     nombre = "bobiConMocos",
---     edad = 10 ,
---     habilidad = ["Sacarse los mocos"],
---     deseos = []
---     }
+chicoPeligroso :: Chico
+chicoPeligroso = Chico "bobiPeligroso" 10 ["Conducir"] [serMayor]
 
+timmy :: Chico
+timmy = Chico "Timmy" 10 ["mirar television", "jugar en la pc"] [serMayor]
+
+taxiDriver :: Chico
+taxiDriver  = Chico "bobiPeligroso" 19 ["Sabe manejar"] []
+
+chicoSinHabilidadCon2Deseos :: Chico
+chicoSinHabilidadCon2Deseos = Chico "bobi" 10 [] [aprenderHabilidades ["Andar en bicicleta"],serMayor]
+-- 1. **Concediendo deseos**
 aprenderHabilidades :: [Habilidades] -> Chico -> Chico
--- aprenderHabilidades nuevasHabilidades unChico = unChico {habilidad = nuevasHabilidades ++ habilidad unChico} 
 aprenderHabilidades nuevasHabilidades unChico = unChico {habilidad = nuevasHabilidades ++ habilidad unChico} 
 
+serMayor :: Chico -> Chico
+serMayor unChico = unChico {edad = 18} 
 -- serGrosoEnNeedForSpeed unChico = aprenderHabilidades 
 
-serMayor unChico = unChico {edad = 18} 
+--  1. wanda: dado un chico, wanda le cumple el primer deseo 
+-- y lo hace madurar (crecer un año de edad).
 
---timmy = Chico “Timmy” 10 [“mirar television”, “jugar en la pc”] [serMayor]
+wanda :: Deseo
+wanda = controlarEdad (+) 1 . cumplirDeseo Uno 
+
+data NumeroDESEO = Uno | Ninguno | Todos 
+cumplirDeseo :: NumeroDESEO -> Deseo
+cumplirDeseo Uno unChico = (head $deseo unChico) unChico  
+cumplirDeseo Ninguno unChico =  unChico  
+cumplirDeseo Todos unChico =  foldl aplicarUnDeseo unChico $deseo unChico  
+-- cumplirDeseo Todos unChico = head $deseo unChico  
+    
+aplicarUnDeseo :: Chico -> Deseo -> Chico
+aplicarUnDeseo unChico unDeseo = unDeseo unChico
+
+controlarEdad :: (Number-> Number-> Number) -> Number ->  Chico -> Chico 
+controlarEdad operacion numero unChico =  unChico {edad = operacion numero (edad unChico)} 
+
+--   2. cosmo: dado un chico, lo hace “des”madurar, quedando con la mitad de años de edad. 
+-- Como es olvidadizo, no le concede ningún deseo.
+
+cosmo :: Deseo
+cosmo = controlarEdad (*) 0.5 . cumplirDeseo Ninguno 
+
+--   3. muffinMagico: dado un chico le concede todos sus deseos. 
+
+muffinMagico :: Deseo
+muffinMagico = cumplirDeseo Todos
+
+-- 2. **En busqueda de pareja** 
+-- 1. tieneHabilidad unaHabilidad unChico : Dado un chico y una habilidad, dice si la posee.
+
+tieneHabilidad :: Habilidades -> Chico -> Bool
+--tieneHabilidad unaHabilidad unChico = elem unaHabilidad $habilidad unChico
+tieneHabilidad unaHabilidad  = elem unaHabilidad . habilidad 
+
+esSuperMaduro :: Chico -> Bool
+esSuperMaduro unChico = tieneHabilidad "Sabe manejar" unChico && ((>18).edad) unChico
+
+-- para Trixie la única condición es que el chico no sea Timmy, 
+
+data Chica = Chica {
+    nombreChica :: String,
+    condicionDeEleccion :: Chico -> Bool
+    } deriving (Show,Eq)
+
+trixie = Chica "Trixie Tang" noEsTimmy
+vicky = Chica "Vicky" (tieneHabilidad "ser un supermodelo noruego") 
+
+noEsTimmy =  (/= "Timmy").nombre  
+noEsTimmy' unChico = nombre unChico /= "Timmy"
+
+-- quienConquistaA :: Chica -> [Chico] -> Chico
+-- quienConquistaA unaChica losPretendientes = head.filter (condicionDeEleccion unaChica) losPretendientes
--- src/Spec.hs
+++ src/Spec.hs
@@ -8,42 +8,38 @@ import Control.Exception (evaluate)
 correrTests :: IO ()
 correrTests = hspec $ do
   suiteDeTestsDeParte1
-  -- suiteDeTestsDeParte2
+  suiteDeTestsDeParte2
   -- suiteDeTestsDeParte3
 suiteDeTestsDeParte1 = describe "Concediendo Deseos" $ do
--- data Chico = Chico {
---     nombre :: String,
---     edad :: Number,
---     habilidad :: [Habilidades],
---     deseos :: [Deseos]
---     } 
-  -- let chico = Chico "bobi" 10 [] []
-  -- let chicoConHabilidad = Chico "bobi" 10 ["Prog Haskell Super Sr"] []
-  -- let chicoMocoso = Chico "bobi" 10 ["Aprender a sacarse los mocos"] []
-
-  let chico = Chico {
-    nombre = "bobi",
-    edad = 10 ,
-    habilidad = []
-    --deseos = []
-    }
-  let chicoConHabilidad  = Chico {
-    nombre = "bobi",
-    edad = 10 ,
-    habilidad = ["Prog Haskell Super Sr"]
-    --deseos = []
-    }
-
-  let chicoMocoso = Chico {
-    nombre = "bobi",
-    edad = 10 ,
-    habilidad = ["Sacarse los mocos"]
-    --deseos = []
-    }
 
   describe "Aprender habilidades" $ do
-    it "Sea una nueva habilidad  sacarse los mocos entonces el chico aprenderá a sacarse los mocos" $ do
-      aprenderHabilidades ["Sacarse los mocos"] chico `shouldBe` chicoMocoso
+    it "Sea una nueva habilidad llamada Conducir entonces un chico aprenderá una nueva habilidad " $ do
+      length (habilidad (aprenderHabilidades ["Conducir"] chico)) `shouldBe`  1
+    it "Sea un chico sin habilidad si no le enseño nada seguira siendo un inutil" $ do
+      length (habilidad (aprenderHabilidades [] chico)) `shouldBe` 0 
     it "Sea un chico con habilidad si no le enseño nada seguira con la misma habilidad" $ do
-      aprenderHabilidades [] chicoConHabilidad `shouldBe` chicoConHabilidad 
-    
+      length (habilidad (aprenderHabilidades [] chicoConHabilidad)) `shouldBe` 1 
+  describe "Mayoria de Edad" $ do
+    it "Sea un niño y le cumplo el deseo de ser Mayor entonces tendrá 18" $ do
+      edad (serMayor chico) `shouldBe` 18 
+
+suiteDeTestsDeParte2 = describe "Padrinos magios" $ do
+  let chicoSinHabilidadCon2Deseos = Chico "bobi" 10 [] [aprenderHabilidades ["Andar en bicicleta"],serMayor]
+  let chicoConPrimerDeseoCumplido = Chico "bobi" 10 ["Andar en bicicleta"] [serMayor]
+  describe "Wanda" $ do
+    it "Dado un chico, wanda le cumple el primer deseo " $ do
+      length (habilidad (wanda chicoSinHabilidadCon2Deseos)) `shouldBe`  1
+    it "Dado un chico, wanda lo hace madurar  " $ do
+      edad (wanda chicoSinHabilidadCon2Deseos) `shouldBe`  11
+  describe "Cosmo" $ do
+    it "Dado un chico, cosmo lo hace desmadurar, quedando con la mitad de años de edad" $ do
+      edad (cosmo chicoSinHabilidadCon2Deseos) `shouldBe`  5
+    it "Como cosmo es olvidadizo, no le concede ningún deseo." $ do
+      length (habilidad (cosmo chicoSinHabilidadCon2Deseos)) `shouldBe`  0
+  describe "muffinMagico" $ do
+    it "Dado un chico le concede todos sus deseos, ser mayor y aprender a bicicletear." $ do
+      length (habilidad(muffinMagico chicoSinHabilidadCon2Deseos)) `shouldBe` 1 
+      edad (muffinMagico chicoSinHabilidadCon2Deseos) `shouldBe`  18 
+
+-- tieneHabilidad "Conducir" chicoPeligroso `shouldBe` True
+-- esSuperMaduro taxiDriver True
